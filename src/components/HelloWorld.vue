b5f37c2557433cd89af661e24f8070a4a0d534b9
Malik Zeeshan <peekinternational@gmail.com>
2023-02-27 03:25:14 -0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <camera
+      :resolution="{ width: 670, height: 477 }"
+      @paused="paused"
+      autoplay
+    ></camera>
+  </div>
+</template>
+
+<script>
+import Camera from "simple-vue-camera";
+export default {
+  components: {
+    Camera,
+  },
+
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
